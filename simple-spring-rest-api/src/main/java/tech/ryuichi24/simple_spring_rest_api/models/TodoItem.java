c42eeb6b3e876fb94b0ba104b362f3adb1bffced
ohnokenichirou <99148127+ohno-kenichirou@users.noreply.github.com>
2024-11-05 08:25:14 +0000

    Spring Boot API 開発

---
+++ simple-spring-rest-api/src/main/java/tech/ryuichi24/simple_spring_rest_api/models/TodoItem.java
@@ -0,0 +1,31 @@
+package tech.ryuichi24.simple_spring_rest_api.models;
+
+public class TodoItem {
+  private int id;
+  private String title;
+
+  public TodoItem() {
+  }
+
+  public TodoItem(int id, String title) {
+    this.id = id;
+    this.title = title;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+}
---
+++ simple-spring-rest-api/src/test/java/tech/ryuichi24/simple_spring_rest_api/SimpleSpringRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.ryuichi24.simple_spring_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
