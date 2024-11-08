2de9f33756a1b4ce4faf57a99b308b5179dc43a5
ohnokenichirou <99148127+ohno-kenichirou@users.noreply.github.com>
2024-11-08 09:27:15 +0000

    データアクセスレイヤーの追加

--- simple-spring-rest-api/src/main/java/tech/ryuichi24/simple_spring_rest_api/models/TodoItem.java
+++ simple-spring-rest-api/src/main/java/tech/ryuichi24/simple_spring_rest_api/models/TodoItem.java
@@ -1,5 +1,11 @@
 package tech.ryuichi24.simple_spring_rest_api.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -8,8 +14,14 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "todos", schema = "business")
 public class TodoItem {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "id", nullable = false)
   private int id;
   @NotBlank(message = "Title must not be blank.")
+  @Column(name = "title", nullable = false)
   private String title;
 }
